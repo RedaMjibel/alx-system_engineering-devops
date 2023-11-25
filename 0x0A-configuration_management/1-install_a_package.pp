54f7db3219aaf2576b96ef8ee6eafaf34135d8d0
RedaMjibel <reda_mjibel@hotmail.com>
2023-11-24 18:54:21 -0800

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,9 @@
 #!/usr/bin/pub
 # puppet manifest installs flask
-package { 'python3':
-  ensure => 'installed',
-}
 
 package { 'Flask':
   ensure   =>  '2.1.0',
   provider =>  'pip3',
-  require  => Package['python3-pip'],
+  ensure   => 'installed',
 }
 
