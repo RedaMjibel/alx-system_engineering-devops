bcd006c0153988b3543b1c38039b5e3dabe33d85
RedaMjibel <reda_mjibel@hotmail.com>
2023-11-25 14:43:03 -0800

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# installs the package puppet-lint
-package { 'Flask':
+# Installs Flask from pip3
+
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
