2853c80b88ce8cf4e5ae022fdb615f88b41ac17c
redamjibel <redamjibel@hotmail.com>
2023-11-24 16:55:02 -0800

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 #creates a file in /tmp
 file { '/tmp/school':
-
   ensure  => 'file',
-  mode    => '0774',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
