a34b1597dcbef7274dc7e642e373baa91283488d
RedaMjibel <reda_mjibel@hotmail.com>
2023-11-24 18:11:52 -0800

    updazt

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # puppet declarative script to install flask from pip3.
 package { 'Flask':
-  ensure  =>  '2.1.0',
-  provider=>  'pip3',
+  ensure   =>  '2.1.0',
+  provider =>  'pip3',
+  require  => Package['python3'],
 }
 
