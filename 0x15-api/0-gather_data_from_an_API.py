80e8a5ae20f2f7fdc4a09516c16be20ab8a69292
RedaMjibel <reda_mjibel@hotmail.com>
2024-01-02 16:48:48 -0800

    restapi

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+returns information about his/her TODO list progress.
+"""
+import requests
+import json
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    mysession = requests.Session()
+    idarg = argv[1]
+    idurl = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idarg)
+    nameurl = 'https://jsonplaceholder.typicode.com/users/{}'.format(idarg)
+
+    empid = mysession.get(idurl)
+    empname = mysession.get(nameurl)
+
+    comp_json = empid.json()
+    name_json = empname.json()['name']
+
+    tasks = 0
+    total_tasks = len(comp_json)
+
+    for tasks_done in comp_json:
+        if tasks_done['completed']:
+            tasks = tasks + 1
+
+    print("Employee {} is done with tasks ({}/{}):".
+          format(name_json, tasks, total_tasks))
+
+    for tasks_done in comp_json:
+        if tasks_done['completed']:
+            print("\t " + tasks_done.get('title'))
