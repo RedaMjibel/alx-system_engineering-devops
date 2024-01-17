f204eb36798c59f41cb2ee5a6598410bae0f19b0
redamjibel <redamjibel@hotmail.com>
2024-01-16 16:58:53 -0800

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
     documentation
     """
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/elbatouri)"
     }
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
