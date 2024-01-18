e3bd61dde735f4533b543db8394cda810c060ea0
reda mjibel <reda_mjibel@hotmail.com>
2024-01-18 02:38:36 +0000

    debug

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# changes the 'phpp' extension to 'php' in the WordPress file
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
